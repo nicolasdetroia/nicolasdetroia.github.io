58b1d4f70645355dd6330e74250ba5bd2949af97
Nicolas DeTroia <102242537+nicolasdetroia@users.noreply.github.com>
2024-10-23 17:45:47 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,59 @@
-window.addEventListener('DOMContentLoaded', event => {
+jQuery(() => {
+  highlight();
+  equalizeFeatureHeight();
+  enableSidebar();
+});
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+/**
+ * Apply the class "active" to the current page in the navigation bar.
+ */
+function highlight() {
+  $("#nav a").each(function() {
+    const self = $(this);
+    const href = self.attr("href");
+    const path = window.location.pathname;
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+    if (path === href) {
+      self.addClass("active");
+      self.siblings().removeClass("active");
+    }
+  });
+}
+
+function equalizeFeatureHeight() {
+  const start = $("#start");
+
+  if (!start.length)
+    return;
+
+  setInterval(() => {
+    let max = 0;
+    const items = start.find(".feature p");
+
+    items.css("height", "auto").each(function() {
+      const h = $(this).height();
+
+      if (h > max)
+        max = h;
     });
 
-});
+    items.height(max);
+  }, 240);
+}
+
+function enableSidebar() {
+  const viewport = $("#viewport");
+
+  $(".lt").on("touchstart click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    viewport.toggleClass("open");
+  });
+
+  $(".inner").on("touchstart click", (e) => {
+    if (viewport.hasClass("open")) {
+      e.stopPropagation();
+      viewport.removeClass("open");
+    }
+  });
+}
